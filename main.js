86253ca6a8a79fb55c31f44d21885eb59499c901
BebbTanner <17tbebb@gmail.com>
2021-09-20 11:32:41 -0700

    assignment 2

---
+++ main.js
@@ -0,0 +1,36 @@
+//question 1
+let myNumber;
+
+//question 2
+myNumber = 5;
+
+//question 2
+let myOtherNumber = 5;
+
+//question 3
+let myTotal = myNumber * myOtherNumber;
+
+//quesiton 5
+let myName = "";
+let myCombo = "";
+myCombo = myNumber * myOtherNumber;
+
+//question 6
+let myOtherCombo;
+ myOtherCombo = myNumber + myOtherNumber;
+
+//question 7
+let myRemainder;
+myRemainder = myNumber % 5;
+
+//question 8
+let i;
+i = 2;
+
+//question 9
+let int;
+int = 4;
+
+//question 10
+let myBoolean;
+myBoolean = false;
